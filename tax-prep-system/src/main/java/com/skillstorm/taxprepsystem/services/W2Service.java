f8ae9682162f50392b89fa2585f1a1d039ea6e39
estein545 <estein545@yahoo.com>
2023-07-24 09:09:48 -0700

    Added folders for service,controller, repository

---
+++ tax-prep-system/src/main/java/com/skillstorm/taxprepsystem/services/W2Service.java
@@ -0,0 +1,21 @@
+package com.skillstorm.taxprepsystem.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skillstorm.taxprepsystem.models.W2;
+import com.skillstorm.taxprepsystem.repositories.W2Repository;
+
+@Service
+public class W2Service {
+
+    @Autowired
+    W2Repository w2Repository;
+
+    public List<W2> findAllBySocial(long social) {
+        return w2Repository.findByIdSocial(social);
+    }
+    
+}
