88d26b540e741ad72876acf6e92abcd3a7cb56ab
estein545 <estein545@yahoo.com>
2023-07-28 11:09:44 -0700

    Fully functional

--- tax-prep-system/src/main/java/com/skillstorm/taxprepsystem/repositories/Ten99Repository.java
+++ tax-prep-system/src/main/java/com/skillstorm/taxprepsystem/repositories/Ten99Repository.java
@@ -3,6 +3,7 @@ package com.skillstorm.taxprepsystem.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.skillstorm.taxprepsystem.models.Ten99;
@@ -10,10 +11,12 @@ import com.skillstorm.taxprepsystem.models.Ten99Id;
 
 @Repository
 public interface Ten99Repository extends MongoRepository<Ten99, Ten99Id> {
-    
+
+    @Query("{'ten99Id.social' : ?0}")
     List<Ten99> findByTen99IdSocial(long social);
 
-    void deleteAllByTen99IdSocial(long social);
+    @Query(value = "{'ten99Id.social' : ?0}", delete = true)
+    List<Ten99> deleteBySocial(long social);
 
     void deleteAllByTen99Id(Ten99Id ten99Id);
 }
